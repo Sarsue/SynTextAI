138096a010ef2a053245a7f5a6107d8192bad200
Osas <osasigbinedion@gmail.com>
2024-10-01 18:02:56 -0400

    fix issues with token in event stream

--- api/routes/sse_route.py
+++ api/routes/sse_route.py
@@ -1,6 +1,6 @@
 from flask import Blueprint,current_app
 from flask_sse import sse
-from utils import get_user_id
+from utils import decode_firebase_token
 sse_bp = Blueprint('sse', __name__)
 
 
@@ -8,7 +8,7 @@ sse_bp = Blueprint('sse', __name__)
 @sse_bp.route('/v1/stream/<token>')
 def stream(token):
     store = current_app.store
-    success, user_info = get_user_id(token)
+    success, user_info = decode_firebase_token(token)
     user_id = store.get_user_id_from_email(user_info['email'])
     # Here you might want to implement logic to filter events by user ID
     return sse.stream(user_id=user_id)
