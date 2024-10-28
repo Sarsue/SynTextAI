d45d2106b935d41fade4271969e6f56568396b1b
Osas <osasigbinedion@gmail.com>
2024-10-28 10:55:34 -0400

    fix the worker ssl issues

--- api/celery_worker.py
+++ api/celery_worker.py
@@ -12,12 +12,12 @@ redis_port = os.getenv("REDIS_PORT")
 def make_celery(app_name):
     celery = Celery(
         app_name,
-        backend=f'rediss://:{redis_pwd}@{redis_host}:{redis_port}/0?ssl_cert_reqs=CERT_NONE',
-        broker=f'rediss://:{redis_pwd}@{redis_host}:{redis_port}/0?ssl_cert_reqs=CERT_NONE'
+        backend=f'rediss://:{redis_pwd}@{redis_host}:{redis_port}/0',
+        broker=f'rediss://:{redis_pwd}@{redis_host}:{redis_port}/0'
     )
     celery.conf.update({
-        'broker_url': f'rediss://:{redis_pwd}@{redis_host}:{redis_port}/0?ssl_cert_reqs=CERT_NONE',
-        'result_backend': f'rediss://:{redis_pwd}@{redis_host}:{redis_port}/0?ssl_cert_reqs=CERT_NONE'
+        'broker_url': f'rediss://:{redis_pwd}@{redis_host}:{redis_port}/0',
+        'result_backend': f'rediss://:{redis_pwd}@{redis_host}:{redis_port}/0'
     })
 
     # Automatically discover tasks from specified modules
